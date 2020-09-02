8ccc9be82ad99fb5f73c7edcace59b421c9e5648
Adrian Piotr Kruszewski <adrian_piotr.kruszewski@contractors.roche.com>
2020-09-02 14:39:06 +0200

    WIP

--- src/kedro_argo/hooks/picle_memory_dataset_hook.py
+++ src/kedro_argo/hooks/picle_memory_dataset_hook.py
@@ -1,16 +1,21 @@
 import itertools
 from glob import glob
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
 from kedro.framework.hooks import hook_impl
 from kedro.io import DataCatalog, MemoryDataSet
 from kedro.pipeline import Pipeline
+from kedro.pipeline.node import Node
 from kedro.extras.datasets.pickle import PickleDataSet
 
 
 class PicleMemoryDataSetHook:
     """Replace all MemoryDataSets with PicleDataSet"""
+
+    def __init__(self, tmp_path: Union[str, Path] = Path("data/tmp")):
+        self._data_sets: Dict[str, Any] = {}
+        self._tmp_path = Path(tmp_path)
 
     @hook_impl
     def before_pipeline_run(
@@ -44,9 +49,42 @@ class PicleMemoryDataSetHook:
                 lambda x: x.inputs, pipeline.nodes
             ))
         )
-
+        
         for dataset_name in filtered_datasets:
             if catalog._data_sets.get(dataset_name) is None:
-                catalog._data_sets[dataset_name] = PickleDataSet(
-                    filepath=f"data/tmp/{dataset_name}.pkl"
+                self._data_sets[dataset_name] = catalog._data_sets[dataset_name] = PickleDataSet(
+                    filepath=(self._tmp_path / f"{dataset_name}.pkl").as_posix()
                 )
+
+    @hook_impl
+    def after_node_run(  # pylint: disable=too-many-arguments
+        self,
+        node: Node,
+        catalog: DataCatalog,
+        inputs: Dict[str, Any],
+        outputs: Dict[str, Any],
+        is_async: bool,
+        run_id: str,
+    ) -> None:
+        """Hook to be invoked after a node runs.
+        The arguments received are the same as those used by ``kedro.runner.run_node``
+        as well as the ``outputs`` of the node run.
+
+        Args:
+            node: The ``Node`` that ran.
+            catalog: A ``DataCatalog`` containing the node's inputs and outputs.
+            inputs: The dictionary of inputs dataset.
+                The keys are dataset names and the values are the actual loaded input data,
+                not the dataset instance.
+            outputs: The dictionary of outputs dataset.
+                The keys are dataset names and the values are the actual computed output data,
+                not the dataset instance.
+            is_async: Whether the node was run in ``async`` mode.
+            run_id: The id of the run.
+        """
+        for name, dataset in self._data_sets.items():
+            if outputs.get(name) is not None:
+                catalog._data_sets[name] = PickleDataSet(
+                    filepath=(self._tmp_path / f"{name}.pkl").as_posix()
+                )
+                catalog._data_sets[name].save(outputs.get(name))
