4446b4dbfca18a584a5ba0fa5fba4f971e3fd8bb
Adrian Piotr Kruszewski <adrian_piotr.kruszewski@contractors.roche.com>
2020-09-14 09:58:38 +0200

    WIP

--- src/kedro_argo/runner/runner.py
+++ src/kedro_argo/runner/runner.py
@@ -1,7 +1,9 @@
 from pathlib import Path
-from typing import Union
+from typing import Any, Dict, Union
 
 from kedro.extras.datasets.pickle import PickleDataSet
+from kedro.io import DataCatalog
+from kedro.pipeline import Pipeline
 from kedro.runner import AbstractRunner, SequentialRunner
 
 
@@ -33,3 +35,55 @@ class ArgoRunner(SequentialRunner):
             for all unregistered data sets.
         """
         return PickleDataSet(filepath=f"{self._tmp_path}/{ds_name}.pkl")
+
+    def run(
+        self, pipeline: Pipeline, catalog: DataCatalog, run_id: str = None
+    ) -> Dict[str, Any]:
+        """Run the ``Pipeline`` using the ``DataSet``s provided by ``catalog``
+        and save results back to the same objects.
+
+        Args:
+            pipeline: The ``Pipeline`` to run.
+            catalog: The ``DataCatalog`` from which to fetch data.
+            run_id: The id of the run.
+
+        Raises:
+            ValueError: Raised when ``Pipeline`` inputs cannot be satisfied.
+
+        Returns:
+            Any node outputs that cannot be processed by the ``DataCatalog``.
+            These are returned in a dictionary, where the keys are defined
+            by the node outputs.
+
+        """
+
+        catalog = catalog.shallow_copy()
+
+        unsatisfied = pipeline.inputs() - set(catalog.list())
+        # If ephemeral dataset exists in _tmp_path add it to catalog,
+        # otherwise raise ValueError.
+        for ds_name in unsatisfied:
+            if (Path(self._tmp_path) / f"{ds_name}.pkl").exists():
+                catalog.add(ds_name, self.create_default_data_set(ds_name))
+            else:
+                raise ValueError(
+                    "Pipeline input(s) {} not found in the "
+                    "DataCatalog".format(unsatisfied)
+                )
+
+        free_outputs = pipeline.outputs() - set(catalog.list())
+        unregistered_ds = pipeline.data_sets() - set(catalog.list())
+        for ds_name in unregistered_ds:
+            catalog.add(ds_name, self.create_default_data_set(ds_name))
+
+        if self._is_async:
+            self._logger.info(
+                "Asynchronous mode is enabled for loading and saving data"
+            )
+        self._run(pipeline, catalog, run_id)
+
+        self._logger.info("Pipeline execution completed successfully.")
+
+        return {ds_name: catalog.load(ds_name) for ds_name in free_outputs}
+
+
